ff69ad89220e4c496f42069939e4885f16329e86
thiagometal <thiagometal.fne@gmail.com>
2018-02-12 20:08:45 -0400

    adiciona componente page header

--- src/about/about.jsx
+++ src/about/about.jsx
@@ -1,7 +1,15 @@
 import React from 'react' 
+import PageHeader from '../template/pageHeader'
 
 export default props => (
     <div>
-        <h1> Sobre </h1>    
+        <PageHeader name='Sobre' small='Nós'> </PageHeader>
+
+        <h2>Nossa História</h2>
+        <p>Lorem ipsum metal core...</p>
+        <h2>Missão e Visão</h2>
+        <p>Missão de dominar o mundo e Visão de trazer os deuses ancestrais para Terra em 100 anos</p>
+        <h2>Imprensa</h2>
+        <p>Lorem ipsum dolor comercial</p>     
     </div>
 )
